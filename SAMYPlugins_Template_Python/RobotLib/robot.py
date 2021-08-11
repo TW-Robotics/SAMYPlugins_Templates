debecba4732df97328e4734357306d1ffc2a7d3e
Felix <leber@technikum-wien.at>
2021-08-11 09:27:37 +0200

    Updated python plugin template

--- SAMYPlugins_Template_Python/RobotLib/robot.py
+++ SAMYPlugins_Template_Python/RobotLib/robot.py
@@ -1,6 +1,14 @@
 
 import logging
+import os
+import sys
+import time
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
+from pubsub import pub
+import CRCL_DataTypes
 
 class Robot(object):
     # This class represents the robot.
@@ -12,7 +20,27 @@ class Robot(object):
         fh = logging.StreamHandler()
         fh.setFormatter(logging.Formatter("%(levelname)s %(filename)s - %(message)s"))
         self.logger.addHandler(fh)
-
         self.host = host
+        
+        # Subscribe to the CRCL command topics
+        pub.subscribe(self.example, "MoveTo")
+        pub.subscribe(self.example2, "SetEndeffector")
+
 
     # Add robot specific methodes here:
+
+    def example(self, data):
+        print("\nGot command:")
+        print(data)
+        pub.sendMessage("command_hold")
+        time.sleep(2)
+        try:
+            print("Running command")
+            # Unkomment this line to test the error handler
+            pub.sendMessage("command_reset")
+        except:
+            pub.sendMessage("command_error")
+
+    def example2(self, data):
+        print("Got command SetEndeffector")
+        print(data)
